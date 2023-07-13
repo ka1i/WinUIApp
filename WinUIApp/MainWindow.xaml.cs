ebc8c0aaa4187e97d382a403076598c893e205e3
ka1i <Mardan137@outlook.com>
2023-07-13 23:18:09 +0800

    customize titlebar styles

--- WinUIApp/MainWindow.xaml.cs
+++ WinUIApp/MainWindow.xaml.cs
@@ -17,25 +17,42 @@ using WinRT.Interop;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace WinUIApp
-{
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            this.InitializeComponent();
-            SetWindowSize(1024, 600);
-        }
+namespace WinUIApp;
 
-        private void SetWindowSize(int width, int height)
-        {
-            IntPtr hWnd = WindowNative.GetWindowHandle(this);
-            var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
-            var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
-            appWindow.Resize(new Windows.Graphics.SizeInt32 { Width = width, Height = height });
-        }
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    internal static int windowWidth = 1280;
+    internal static int windowHeight = 720;
+    internal static string windowTitle = "WinUI App";
+
+    internal static string appVersion = "v0.0.1";
+    internal static string appResolution = string.Format("({0}x{1})", windowWidth, windowHeight);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        Title = windowTitle;
+
+        var hWnd = WindowNative.GetWindowHandle(this);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        appWindow?.Resize(new Windows.Graphics.SizeInt32 { Width = windowWidth, Height = windowHeight });
+
+        AppTitleTextBlock.Text = windowTitle;
+        AppVersionTextBlock.Text = appVersion;
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
+    }
+
+    private void WindowSizeChanged(object sender, WindowSizeChangedEventArgs args)
+    {
+        var hWnd = WindowNative.GetWindowHandle(this);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+        var appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+        AppResolutionTextBlock.Text = string.Format("({0}x{1})", appWindow.Size.Width, appWindow.Size.Height);
     }
 }
